cba82d94300a1a807508e2582699d9ec15f0d4df
TRNisbet <TRNisbet@hotmail.com>
2023-01-24 13:36:23 -0500

    needs table and styling

--- 1/08-Stu_Stats_Functions/Unsolved/static/js/app_starter.js
+++ 1/08-Stu_Stats_Functions/Unsolved/static/js/app_starter.js
@@ -1,1 +1,14 @@
 var movieScore = [4.4, 3.3, 5.9, 8.8, 1.2, 5.2, 7.4, 7.5, 7.2, 9.7, 4.2, 6.9];
+
+
+function avg(arr) {
+    var total = 0;
+        for (var i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+    var meanValue = total / arr.length;
+
+  return meanValue;
+}
+
+console.log(`The mean is : ${avg(movieScore)}`);
--- 2/03-Stu_Word_Counter_Objects/Unsolved/index_starter.js
+++ 2/03-Stu_Word_Counter_Objects/Unsolved/index_starter.js
@@ -1,12 +1,21 @@
 // Initialize the function
-
+function wordCount(sentence){
   // Convert string to an array of words
-  
+  var sentenceString = sentence.split(" ");
 
   // An object to hold word frequency
+  var totalCharacters = {};
 
-
+  sentenceString.forEach(function(word){
+    if(word in totalCharacters){
+      totalCharacters[word] += 1; 
+    }else{
+      totalCharacters[word] =1; 
+    }
+  })
   // Iterate through the array and count the occurrence of each word
-
-
+  console.log(totalCharacters);
+return(totalCharacters);
+}
 //  Call the function with the string as a parameter.
+wordCount("I yam what I yam and always will be what I yam");
